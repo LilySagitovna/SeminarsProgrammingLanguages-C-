41a42b36810ef649aee3de774527fa6cfeb6ed33
Lily <ms.lilichka@yandex.ru>
2022-07-18 23:34:02 +0500

    Семинар задача 5

---
+++ Seminar2/Zd5/Program.cs
@@ -0,0 +1,21 @@
+﻿try
+{
+    int a=int.Parse(Console.ReadLine()!);
+}
+catch (System.Exception e)
+{
+    Console.WriteLine("Не верно!");
+    Console.WriteLine(e);
+    throw;
+}
+//////////////////////////////////////////////////////////////
+Console.WriteLine("Введите первое число");
+int number = int.Parse(Console.ReadLine()!);
+
+while (!int.TryParse(Console.ReadLine(), out number))
+{
+    System.Console.WriteLine("Введите корректное значение!");
+}
+System.Console.WriteLine(number);
+
+///////////////////////////////////////////////////
