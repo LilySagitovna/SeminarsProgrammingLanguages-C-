738f6ee4cf6f099929db7b56e1432a758fae4666
Lily <ms.lilichka@yandex.ru>
2022-07-18 23:30:52 +0500

    Семинар задача 3

---
+++ Seminar1/Zadacha3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Принимает на вход число n,а на выход выдает все числа от -n до +n
+System.Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+System.Console.WriteLine("Ответ:");
+
+while (count <= number)
+{
+    System.Console.Write($"{count}, ");
+    count++;
+}
+
+
+
+
+
+
+
+
+
+
+
