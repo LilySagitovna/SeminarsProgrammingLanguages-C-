7845a91221f2e57ea7bf18b1af27002b889b51f9
Lily <ms.lilichka@yandex.ru>
2022-07-18 23:40:21 +0500

    Задача с семинара 2

---
+++ Seminar4/Z2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// string number = Console.ReadLine();
+// int digits = number.Length;
+// Console.WriteLine(digits);
+
+int number = Convert.ToInt32(Console.ReadLine());
+int Count = 0;
+while(number != 0)
+{
+    number /= 10;
+    Count++;
+}
+Console.WriteLine(Count);
