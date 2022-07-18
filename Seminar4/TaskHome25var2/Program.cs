f0596f4306df4f8b68de0320c5d9cbf7270fafe0
Lily <ms.lilichka@yandex.ru>
2022-07-18 23:38:11 +0500

    ДЗ задача  25 вар2

---
+++ Seminar4/TaskHome25var2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+//  и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+/* System.Console.WriteLine("Введите числа через запятую: ");
+string[] numbers = Console.ReadLine()!.Split(',');
+int num = int.Parse(numbers[0]);
+int degree = int.Parse(numbers[1]);
+for(;degree<1;)
+{
+        System.Console.WriteLine($"Внимание! Введите заново основание степени" +
+                $" и натуральный показатель степени > 0: ");
+        numbers = Console.ReadLine()!.Split(',');
+        num = int.Parse(numbers[0]);
+        degree = int.Parse(numbers[1]);    
+}
+    Console.WriteLine($"Возведение числа {num}" +
+        $" в натуральную степень {degree}" +
+        $" равна {Math.Pow(num, degree)}");
+ */
+///////////////////////////////
+// System.Console.WriteLine("Введите числа через запятую: ");
+// string[] numbers = Console.ReadLine()!.Split(',');
+// int num = int.Parse(numbers[0]);
+// int degree = int.Parse(numbers[1]);
+
+
+// while(degree<1)
+// {
+//         System.Console.WriteLine($"Внимание! Введите заново основание степени" +
+//                 $" и натуральный показатель степени > 0: ");
+//         numbers = Console.ReadLine()!.Split(',');
+//         num = int.Parse(numbers[0]);
+//         degree = int.Parse(numbers[1]);    
+// }
+
+//     Console.WriteLine($"Возведение числа {num}" +
+//         $" в натуральную степень {degree}" +
+//         $" равна {Math.Pow(num, degree)}");
+/////////////////////////////////////////////
