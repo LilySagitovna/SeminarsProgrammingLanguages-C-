f01f11194082788ecf2a298f496eafaacc6681d1
Lily <ms.lilichka@yandex.ru>
2022-07-18 23:36:19 +0500

    Задача 2 с семинара

---
+++ Seminar3/Z2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите четверть:");
+int number = Convert.ToInt32(Console.ReadLine());
+if ( number == 1)
+{
+    Console.WriteLine("X > 0,Y > 0");
+    }
+    if ( number == 2)
+{
+    Console.WriteLine("X < 0,Y > 0");
+    }
+if ( number == 3)
+{
+    Console.WriteLine("X < 0,Y < 0");
+    }
+    if ( number == 4)
+{
+    Console.WriteLine("X > 0,Y < 0");
+    }
+    
