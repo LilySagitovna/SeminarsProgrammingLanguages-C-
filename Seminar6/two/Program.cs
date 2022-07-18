981578d0ff8de802879bc1d6ed8789a95179b712
Lily <ms.lilichka@yandex.ru>
2022-07-18 23:48:26 +0500

    Задачи с семинара

---
+++ Seminar6/two/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 40: Напишите программу, которая принимает
+//  на вход три числа и проверяет, может ли существовать 
+//  треугольник с сторонами такой длины.
+int numbersOne = int.Parse(Console.ReadLine()!);
+int numbersTwo = int.Parse(Console.ReadLine()!);
+int numbersThree = int.Parse(Console.ReadLine()!);
+
+//bool result =  numbersOne<=(numbersTwo + numbersThree) true: false;
+if (numbersOne<(numbersTwo + numbersThree) && numbersTwo<(numbersOne + numbersThree) 
+    && numbersThree<(numbersTwo + numbersOne))
+{
+    System.Console.WriteLine("Может");
+}
+else
+{
+    System.Console.WriteLine("Не может");
+}
