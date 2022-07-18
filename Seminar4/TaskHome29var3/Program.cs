f1bf51f3bcf6fb2311057d793a521c1e51c2cfcd
Lily <ms.lilichka@yandex.ru>
2022-07-18 23:39:36 +0500

    ДЗ задача  29 вар3

---
+++ Seminar4/TaskHome29var3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: Напишите программу, которая задаёт
+//  массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] Array = PrintArray(8);
+System.Console.WriteLine($"{String.Join(",",Array)}");
+int[] PrintArray(int length)
+{
+    int[] Print = new int [length];
+    for (int i = 0; i < length; i++)
+    {
+        Print[i]=new Random().Next(65,351);
+    }
+    return Print;
+}
