fed94ac6fd97926112ca5598bd6a8451a3dd46a7
Lily <ms.lilichka@yandex.ru>
2022-07-18 23:29:45 +0500

    ДЗ задача 4

---
+++ Seminar1/DZ_Zadacha4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//for (int index = 2; index <= number; index += 2)
+//   System.Console.WriteLine(index);
+
+int index = 2;
+System.Console.WriteLine("Все четные числа: ");
+while (index <= number)
+{
+    System.Console.Write($"{index},");
+    index += 2;
+}
+System.Console.WriteLine("\b \b");
