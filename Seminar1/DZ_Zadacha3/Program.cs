0a5214a797b23bc911fffc20971745418edd174b
Lily <ms.lilichka@yandex.ru>
2022-07-18 23:29:32 +0500

    ДЗ задача 3

---
+++ Seminar1/DZ_Zadacha3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+System.Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number % 2 == 0)
+{
+    System.Console.WriteLine("Является четным");
+}
+else
+{
+    System.Console.WriteLine("Не является четным");
+}
+
+
+
