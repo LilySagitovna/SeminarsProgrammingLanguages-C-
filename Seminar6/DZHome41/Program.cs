5937b3d477fec5f60f3cc8816daebf2e0bf2d333
Lily <ms.lilichka@yandex.ru>
2022-07-18 23:46:06 +0500

    ДЗ 41 вар 1

---
+++ Seminar6/DZHome41/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 4 */
+
+
+Console.WriteLine("Введите числа через запятую");
+string[] text = Console.ReadLine()!.Split(',');
+int count = 0;
+for (int i = 0; i < text.Length; i++)
+{
+    int numbers = int.Parse(text[i]);
+    if (numbers > 0) count++;
+}
+System.Console.WriteLine($"{count} чисел больше 0");
+
+
