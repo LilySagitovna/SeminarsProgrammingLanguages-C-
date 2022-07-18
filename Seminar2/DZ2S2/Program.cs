90811d6cb71b5116d9062fd9a81fa8486ba127e6
Lily <ms.lilichka@yandex.ru>
2022-07-18 23:32:28 +0500

    Семинар 2 дз  задача 2

---
+++ Seminar2/DZ2S2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+
+System.Console.WriteLine("Введите число: ");
+string number = Console.ReadLine()!;
+if (int.Parse(number) > 99)
+{
+    System.Console.WriteLine($"Третья цифра числа: {number[2]}");
+}
+else
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+
+// System.Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// string index = number.ToString();
+// if (number > 99)
+// {
+//     System.Console.WriteLine($"Третья цифра числа: {index[2]}");
+// }
+// else
+// {
+//     System.Console.WriteLine("Третьей цифры нет");
+// }
