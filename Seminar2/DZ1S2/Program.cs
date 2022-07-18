03726c5b1cc2311edb40af1ae17709f904c8c344
Lily <ms.lilichka@yandex.ru>
2022-07-18 23:32:08 +0500

    Семинар 2 дз  задача 1

---
+++ Seminar2/DZ1S2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// System.Console.WriteLine("Введите трехзначное число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// int secondNumber = number / 10 % 10;
+// System.Console.WriteLine($"Вторая цифра трехзначного числа: {secondNumber}");
+
+System.Console.WriteLine("Введите трехзначное число: ");
+string number = Console.ReadLine()!;
+System.Console.WriteLine($"Вторая цифра трехзначного числа: {number[1]}");
