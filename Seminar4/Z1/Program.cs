6d48765c8230447337eb55bf6337b371563bbf9e
Lily <ms.lilichka@yandex.ru>
2022-07-18 23:40:11 +0500

    Задача с семинара 1

---
+++ Seminar4/Z1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 24: Напишите программу, 
+// которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10: 1+2+3+4 = 10
+// 8 -> 36
+
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+// while, for
+
+for (int i = 1; i<=number; i++)
+{
+    sum += i;
+}
+
+Console.WriteLine($"Cумма чисел: {sum}");
