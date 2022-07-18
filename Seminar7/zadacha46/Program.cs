71b9beff1ab88ef33b923bde2cddd687131df602
Lily <ms.lilichka@yandex.ru>
2022-07-18 23:50:08 +0500

    Решение на семинаре задача 46

---
+++ Seminar7/zadacha46/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int rows = int.Parse(Console.ReadLine()!);
+int columns = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = new int[rows,columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(11);
+        // [0;11) -> [0;10]
+        Console.Write(matrix[i,j] + "\t"); // Литералы
+    }
+    Console.WriteLine();
+}
