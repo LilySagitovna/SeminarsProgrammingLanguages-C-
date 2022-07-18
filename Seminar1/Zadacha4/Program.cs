c14f5dd97d65e8cdaf62c052d43114d1758a32a8
Lily <ms.lilichka@yandex.ru>
2022-07-18 23:31:04 +0500

    Семинар задача 4

---
+++ Seminar1/Zadacha4/Program.cs
@@ -0,0 +1,11 @@
+﻿//Программа принимает на вход число,а выдает последнюю цифру числа 456 (6)
+System.Console.WriteLine("Введите трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number < 1000)
+{
+    System.Console.WriteLine(number % 10);
+}                    
+else
+{
+    System.Console.WriteLine("Error");
+}
