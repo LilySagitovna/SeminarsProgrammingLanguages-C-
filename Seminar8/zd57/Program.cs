ca75b747a2938931dc4de844773781623b8d992a
Lily <ms.lilichka@yandex.ru>
2022-07-23 10:31:46 +0500

    Решение с семинара зд 57

---
+++ Seminar8/zd57/Program.cs
@@ -0,0 +1,69 @@
+﻿/* Задача 57: Составить частотный словарь элементов 
+двумерного массива. Частотный словарь содержит 
+информацию о том, сколько раз встречается элемент входных данных.
+
+Набор данных
+Частотный массив
+{ 1, 9, 9, 0, 2, 8, 0, 9 }
+0 встречается 2 раза
+1 встречается 1 раз
+2 встречается 1 раз
+8 встречается 1 раз
+9 встречается 3 раза
+1, 2, 3
+4, 6, 1
+2, 1, 6
+1 встречается 3 раза
+2 встречается 2 раз
+3 встречается 1 раз
+4 встречается 1 раз
+6 встречается 2 раза
+
+Диапазон чисел [0, 9]
+ */
+int[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Search(int[,] matr)
+{
+    int[] count = new int[10];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            count[matr[i, j]]++;
+        }
+    }
+    for (int k = 0; k < count.Length; k++)
+    {
+        Console.WriteLine($"Число {k}  встречается {count[k]} раз");
+    }
+}
+
+int[,] array = GetMatrix(4, 4, 0, 10);
+PrintMatrix(array);
+Console.WriteLine();
+Search(array);
